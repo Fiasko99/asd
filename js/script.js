04fe847fbd4ed0e028e73c3e57113efe54460e3d
Fiasko99 <snakeice2000@mail.ru>
2021-12-20 16:04:38 +0300

    dserg

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+  const header = document.getElementById('header')
+  header.innerHTML = 
+  `
+    <div class="nav">
+      <div class="first_link"><a href="./index.html">Главная</a></div>
+      <div class="first_link"><a href="./about.html">Обо мне</a></div>
+    </div>
+  ` 
+}
